e9bd913ccd503a34f9fbfa13c87e342e5bf9a3df
Michael Kuhn <mkuhn@nrel.gov>
2025-03-11 16:15:17 -0600

    updated tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -36,6 +36,9 @@ int main(int argc, char *argv[]) {
   fftw_complex *eta_modes =
       modes_hosgrid::allocate_plan_copy(n0, n1, plan, mFS);
 
+  std::vector<fftw_plan> plan_vector{};
+  plan_vector.push_back(plan);
+
   // Allocate ptrs for velocity as well, copy is built-in later
   auto u_modes = modes_hosgrid::allocate_complex(n0, n1);
   auto v_modes = modes_hosgrid::allocate_complex(n0, n1);
@@ -107,7 +110,8 @@ int main(int argc, char *argv[]) {
 
   // Perform fftw for eta
   amrex::Gpu::DeviceVector<amrex::Real> hos_eta_vec(n0 * n1, 0.0);
-  modes_hosgrid::populate_hos_eta(n0, n1, dimL, plan, eta_modes, hos_eta_vec);
+  modes_hosgrid::populate_hos_eta(n0, n1, dimL, plan_vector, eta_modes,
+                                  hos_eta_vec);
 
   // Loop through heights to check and print
   int n_hvec = 0;
@@ -148,9 +152,9 @@ int main(int argc, char *argv[]) {
     // Get sample height
     amrex::Real ht = hvec[indvec[iht]];
     // Sample velocity
-    modes_hosgrid::populate_hos_vel(n0, n1, xlen, ylen, depth, ht, dimL, dimT,
-                                    mX, mY, mZ, plan, u_modes, v_modes, w_modes,
-                                    hos_u_vec, hos_v_vec, hos_w_vec, indv);
+    modes_hosgrid::populate_hos_vel(
+        n0, n1, xlen, ylen, depth, ht, dimL, dimT, mX, mY, mZ, plan_vector,
+        u_modes, v_modes, w_modes, hos_u_vec, hos_v_vec, hos_w_vec, indv);
     indv += n0 * n1;
   }
 
@@ -159,10 +163,11 @@ int main(int argc, char *argv[]) {
   const amrex::Real spd_dy = ylen / n1;
   const amrex::Real zero_sea_level = 0.0;
   interp_to_mfab::interp_eta_to_levelset_field(
-      n0, n1, spd_dx, spd_dy, zero_sea_level, hos_eta_vec, phi_field, geom_all);
-  interp_to_mfab::interp_velocity_to_field(n0, n1, spd_dx, spd_dy, indvec, hvec,
-                                           hos_u_vec, hos_v_vec, hos_w_vec,
-                                           velocity_field, geom_all);
+      n0, n1, spd_dx, spd_dy, 0.0, 0.0, zero_sea_level, true, hos_eta_vec,
+      phi_field, geom_all);
+  interp_to_mfab::interp_velocity_to_field(
+      n0, n1, spd_dx, spd_dy, 0.0, 0.0, true, indvec, hvec, hos_u_vec,
+      hos_v_vec, hos_w_vec, velocity_field, geom_all);
 
   // Try to get next timestep (not available)
   bool readflag = rmodes.get_data(2.0 * dt_out, mX, mY, mZ, mFS);
@@ -181,6 +186,7 @@ int main(int argc, char *argv[]) {
   delete[] u_modes;
   delete[] v_modes;
   delete[] w_modes;
+  plan_vector.clear();
   fftw_destroy_plan(plan);
 
   // Finalize AMReX
